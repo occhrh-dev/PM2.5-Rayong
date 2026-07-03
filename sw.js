68f55bbf4a2ba62e25c173034024b66e1c4f406e
occhrh-dev <occ.hrh@gmail.com>
2026-07-03 22:06:51 +0700

    Add methodology/about page for academic referencing: sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // PM2.5 @RY service worker
-const VERSION='v6';
+const VERSION='v7';
 const CORE=['./','index.html','rayong_tambon.geojson','data/centroids.json','manifest.json'];
 
 self.addEventListener('install',e=>{
